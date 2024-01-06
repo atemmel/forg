9c4f4313095df2fc3df338864604ed42763662bc
atemmel <temmeladam@gmail.com>
2024-01-06 04:23:31 +0100

    Updated test script

--- core/run-tests.sh
+++ core/run-tests.sh
@@ -4,7 +4,8 @@ shopt -s globstar
 
 SRC=$(ls src/**/*.cpp)
 TEST_SRC=$(ls tests/**/*.cpp)
+TEST_FLAGS="-g -O0 --coverage"
 
-g++ -o test -g -O0 -Iinclude -lCatch2Main $SRC $TEST_SRC -lCatch2 --coverage
+g++ -o test $TEST_FLAGS -Iinclude -lCatch2Main $SRC $TEST_SRC -lCatch2
 
 # gcovr
